598064aa0f3c5de3fe9dfccef3003521ef5b750f
Nguyen Van Say <vansay@hotmail.fr>
2020-03-05 17:26:13 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,6 +5,6 @@ describe "current_age_for_birth_year method" do
     current_year = Time.now.year
     birth_year = 1984
     answer = current_year - birth_year
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(answer)
   end
 end
